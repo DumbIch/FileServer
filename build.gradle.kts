2b508c71d951321973d33d699b69d227f9217c1b
dumdumbich <dmdumbich@yandex.ru>
2023-04-11 09:30:47 +0300

    Create test web-server with Ktor

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,8 @@
+val kotlinVersion: String by project
+val ktorVersion: String by project
+val logbackVersion: String by project
+
+
 plugins {
     kotlin("jvm") version "1.8.0"
     application
@@ -11,6 +16,18 @@ repositories {
 }
 
 dependencies {
+    // Ktor
+    implementation("io.ktor:ktor-server-core-jvm:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty-jvm:$ktorVersion")
+    testImplementation("io.ktor:ktor-server-tests-jvm:$ktorVersion")
+
+    // LogBack
+    implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+    // Kotlin
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlinVersion")
+
+    // Test
     testImplementation(kotlin("test"))
 }
 
@@ -23,12 +40,15 @@ kotlin {
 }
 
 application {
-    mainClass.set("ru.dumdumbich.MainKt")
+    mainClass.set("ru.dumdumbich.ApplicationKt")
+
+    val isDevelopment: Boolean = project.ext.has("development")
+    applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
 }
 
 tasks.jar {
     manifest {
-        attributes["Main-Class"] = "ru.dumdumbich.MainKt"
+        attributes["Main-Class"] = "ru.dumdumbich.ApplicationKt"
     }
     configurations["compileClasspath"].forEach { file: File ->
         from(zipTree(file.absoluteFile))
